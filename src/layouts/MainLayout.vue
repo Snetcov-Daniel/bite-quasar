f37d7fec8631b6dbfdf126aa60bc552f8a1cd7e8
Snetcov Daniel <snetcovdaniel@gmail.com>
2022-06-18 15:30:15 +0300

    made layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,14 @@
 <template>
-    <q-layout>
+    <q-layout hHh lpr fff>
+        <q-header reveal bordered>
 
+        </q-header>
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+        <q-footer reveal bordered>
+
+        </q-footer>
     </q-layout>
 </template>
 
