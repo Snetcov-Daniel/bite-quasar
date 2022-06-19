6172c2bb0c66a7941894a310f694cbb6201a9cf0
Varzari Eugeniu <vzeugeniu@gmail.com>
2022-06-19 14:40:35 +0300

    Get Started Tab

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,19 @@
 <template>
     <q-page>
+        <get-started/>
         <description/>
     </q-page>
 </template>
 
 <script>
 import Description from '../components/Description'
+import GetStarted from '../components/Get-started'
 
 export default {
     name: 'PageIndex',
     components: {
-        description: Description
+        description: Description,
+        getStarted: GetStarted
     }
 }
 </script>
